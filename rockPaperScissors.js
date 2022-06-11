a0155f3ab2716927576d68f126ae721cd615d49b
Deandre Clark <Deandreclark123@@gmail.com>
2022-06-11 11:24:36 -0400

    add game function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,24 +5,61 @@ let computerPlay = () =>{
 };
 
 let playRound = (playerSelection, computerSelection = computerPlay()) =>{
+    let playerInput;
     dict = {
         "Rock": ["Paper","Scissior"],
         "Paper": ["Scissior", "Rock"],
         "Scissior": ["Rock", "Paper"],
 
     };
-    console.log("the computer chose " + computerSelection);
-    console.log("you chose " + playerSelection);
-    if(dict[playerSelection][0] === computerSelection){
-        return "You lose, " + playerSelection + " beats " + computerSelection;
+    switch(playerSelection.toLowerCase()){
+        case "rock":
+            playerInput = "Rock";
+            break;
+        case "paper":
+            playerInput = "Paper";
+            break;
+        case "scissior":
+            playerInput = "Scissior";
+            break;
     }
-    else if(dict[playerSelection][1] === computerSelection){
-        return "You win, " + playerSelection + " beats " + computerSelection;
+    console.log("the computer chose " + computerSelection);
+    console.log("you chose " + playerInput);
+    if(dict[playerInput][0] === computerSelection){
+        return false;
+    }
+    else if(dict[playerInput][1] === computerSelection){
+        return true;
     }
     else{
-        return "you tied.";
+        return null;
     }
 };
 
-console.log(computerPlay());
-console.log(playRound("Scissior"));
+let game = () =>{
+    comp = 0;
+    player = 0
+
+    for(let i = 0; i<5; i++){
+        let input = prompt("Enter rock, paper, or scissior: ", "rock")
+        result = playRound(input)
+        if(result){
+            player +=1;
+        }
+        else if(result === false){
+            comp +=1;
+        }
+        console.log("Score, player: " + player +" computer: " + comp);
+    }
+    console.log("last out put player " + player + " - " +comp)
+    
+    if(player > comp){
+        return "You Win!"
+    }
+    if(player === comp){
+        return "Tie"
+    }
+    return "You Lose."
+};
+
+console.log(game());
